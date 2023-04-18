4cde6de76bd7f11a09b48623f95a7d2424cc3ae3
Lee Weber <weber21w@gmail.com>
2023-04-17 22:17:22 -0500

    Update cu_mouse.c

--- cu_mouse.c
+++ cu_mouse.c
@@ -43,32 +43,31 @@ void cu_adjust_mouse_scale(){ /* TODO replace the implementation in main.c, */
 }
 
 
-uint8 cu_mouse_encode_delta(int d){
-	uint8 result;
-	if (d < 0)
-	{
-		result = 0;
-		d = -d;
-	}
-	else
-		result = 1;
-	if (d > 127)
-		d = 127;
-	if (!(d & 64))
-		result |= 2;
-	if (!(d & 32))
-		result |= 4;
-	if (!(d & 16))
-		result |= 8;
-	if (!(d & 8))
-		result |= 16;
-	if (!(d & 4))
-		result |= 32;
-	if (!(d & 2))
-		result |= 64;
-	if (!(d & 1))
-		result |= 128;
-	return result;
+auint cu_mouse_encode_delta(int d){
+ uint8 result;
+ if (d < 0){
+   result = 0;
+   d = -d;
+ }else
+  result = 1;
+
+ if (d > 127)
+  d = 127;
+ if (!(d & 64))
+  result |= 2;
+ if (!(d & 32))
+  result |= 4;
+ if (!(d & 16))
+  result |= 8;
+ if (!(d & 8))
+  result |= 16;
+ if (!(d & 4))
+  result |= 32;
+ if (!(d & 2))
+  result |= 64;
+ if (!(d & 1))
+  result |= 128;
+ return result & 0xFF;
 }
 
 
@@ -77,14 +76,14 @@ void cu_mouse_update(){
 // if (cu_mouse_enabled){
 //  if ((ev->type) == SDL_MOUSEMOTION){ /* http://www.repairfaq.org/REPAIR/F_SNES.html we always report "low sensitivity" */
 
-//	cu_mouse_buttons = SDL_GetRelativeMouseState(&cu_mouse_dx,&cu_mouse_dy);
-//	cu_mouse_dx >>= cu_mouse_scale;
-//	cu_mouse_dy >>= cu_mouse_scale;
+// cu_mouse_buttons = SDL_GetRelativeMouseState(&cu_mouse_dx,&cu_mouse_dy);
+// cu_mouse_dx >>= cu_mouse_scale;
+// cu_mouse_dy >>= cu_mouse_scale;
 
-//	if (guicore_getflags() & 0x0001U){ /* GUICORE_FULLSCREEN HACK TODO */
-//		SDL_WarpMouseInWindow(guicore_window,400,300); /* keep mouse centered so it doesn't get stuck on the edge of the screen... */
-//		SDL_GetRelativeMouseState(&cu_mouse_dx,&cu_mouse_dy); /*...and immediately consume the bogus motion event it generated. */
-//	}
+// if (guicore_getflags() & 0x0001U){ /* GUICORE_FULLSCREEN HACK TODO */
+//  SDL_WarpMouseInWindow(guicore_window,400,300); /* keep mouse centered so it doesn't get stuck on the edge of the screen... */
+//  SDL_GetRelativeMouseState(&cu_mouse_dx,&cu_mouse_dy); /*...and immediately consume the bogus motion event it generated. */
+// }
 //  }
 // }
 
@@ -94,11 +93,53 @@ void cu_mouse_update(){
 
 auint cu_mouse_process(auint prev, auint curr){
 /*
-	buttons[0] = (encode_delta(cu_mouse_dx) << 24) | (encode_delta(cu_mouse_dy) << 16) | 0x7FFF;
-	if (cu_mouse_buttons & SDL_BUTTON_LMASK)
-		buttons[0] &= ~(1<<9);
-	if (cu_mouse_buttons & SDL_BUTTON_RMASK)
-		buttons[0] &= ~(1<<8);
+ buttons[0] = (encode_delta(cu_mouse_dx) << 24) | (encode_delta(cu_mouse_dy) << 16) | 0x7FFF;
+ if (cu_mouse_buttons & SDL_BUTTON_LMASK)
+  buttons[0] &= ~(1<<9);
+ if (cu_mouse_buttons & SDL_BUTTON_RMASK)
+  buttons[0] &= ~(1<<8);
 */
-	return curr;
+ return curr;
+}
+
+
+auint  cu_mouse_get_enabled(void){
+ return cu_mouse_enabled;
+}
+
+
+void cu_mouse_set_enabled(uint8 val){
+ cu_mouse_enabled = val;
+}
+
+
+auint  cu_mouse_get_buttons(void){
+ return cu_mouse_buttons;
+}
+
+
+void cu_mouse_set_buttons(uint8 val){
+ cu_mouse_buttons = val;
+}
+
+
+auint  cu_mouse_get_scale(void){
+ return cu_mouse_scale;
+}
+
+
+void cu_mouse_set_scale(uint8 val){
+ cu_mouse_scale = val;
+}
+
+
+void  cu_mouse_get_position(sint32 *mx, sint32 *my){
+ *mx = cu_mouse_dx;
+ *my = cu_mouse_dy;
+}
+
+
+void cu_mouse_set_position(sint32 mx, sint32 my){
+ cu_mouse_dx = mx;
+ cu_mouse_dy = my;
 }
