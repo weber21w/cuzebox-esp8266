ec4bf57e5829b405b9e063bfa26831d4819ea1ba
Lee Weber <weber21w@gmail.com>
2023-04-17 22:17:46 -0500

    Update cu_mouse.h

--- cu_mouse.h
+++ cu_mouse.h
@@ -35,8 +35,59 @@ sint32 cu_mouse_dx, cu_mouse_dy;
 uint8 cu_mouse_buttons;
 uint8 cu_mouse_scale;
 
-extern void cu_adjust_mouse_scale();
+void cu_adjust_mouse_scale();
+
+
 void cu_mouse_update();
+
+
 auint cu_mouse_process(auint prev, auint curr);
+
+
+/*
+** Get the mouse enabled status
+*/
+auint  cu_mouse_get_enabled(void);
+
+
+/*
+** Set the mouse status to enabled
+*/
+void  cu_mouse_set_enabled(uint8 val);
+
+
+/*
+** Get the mouse position
+*/
+void  cu_mouse_get_position(sint32 *mx, sint32 *my);
+
+
+/*
+** Set the mouse position
+*/
+void  cu_mouse_set_position(sint32 mx, sint32 my);
+
+
+/*
+** Get the mouse buttons
+*/
+auint  cu_mouse_get_buttons(void);
+
+
+/*
+** Set the mouse buttons
+*/
+void  cu_mouse_set_buttons(uint8 val);
+
+
+
+/*
+** Get the mouse scale
+*/
+auint  cu_mouse_get_scale(void);
+/*
+** Set the mouse scale
+*/
+void  cu_mouse_set_scale(uint8 val);
 
 #endif /* MOUSE_H */
