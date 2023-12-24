f87d3ab9246fa03954df4bc6da7ea4670865f495
Lee Weber <weber21w@gmail.com>
2023-12-23 23:10:40 -0600

    Added JAMMA options and Icon handling

--- cu_ufile.c
+++ cu_ufile.c
@@ -49,6 +49,15 @@ static const char cu_war[] = "Warning: ";
 #define PERIPHERAL_MULTITAP 4
 #define PERIPHERAL_ESP8266 8
 
+#define JAMMA_ROTATE_90 1
+#define JAMMA_ROTATE_180 2
+#define JAMMA_ROTATE_270 4
+#define JAMMA_FLIP_H 8
+#define JAMMA_FLIP_V 16
+#define JAMMA_B0 32 //future use...
+#define JAMMA_B1 64
+#define JAMMA_B2 128
+
 /*
 ** Attempts to load the passed file into code memory. The code memory is not
 ** cleared, so bootloader image or other contents may be added before this if
@@ -66,6 +75,8 @@ boole cu_ufile_load(char const* fname, uint8* cmem, cu_ufile_header_t* head)
  auint len;
  sint8 psupport_str[256];
  sint8 pdefault_str[256];
+ sint8 pdisplay_str[256*2];
+ sint8 jamma_str[256];
 
  if (!filesys_open(FILESYS_CH_EMU, fname)){
   print_error("%s%sCouldn't open %s.\n", cu_id, cu_err, fname);
@@ -110,49 +121,64 @@ boole cu_ufile_load(char const* fname, uint8* cmem, cu_ufile_header_t* head)
               ((auint)(buf[13]) <<  8);
 
  for (i = 0U; i < 31U; i++){ head->name[i] = buf[14U + i]; }
- head->name[31] = 0U;
+  head->name[31] = 0U;
 
  for (i = 0U; i < 31U; i++){ head->author[i] = buf[46U + i]; }
- head->author[31] = 0U;
+  head->author[31] = 0U;
 
  auint icon_crc = 0;
  for (i = 0U; i < 256U; i++){
   head->icon[i] = buf[78U + i];
   icon_crc += head->icon[i];
  }
-/*
- if (icon_crc){
-  uint8 x,y;
-  for (y = 0U; y < 8U; y++){
-   for (x = 0U; x < 8U; x++){
-  
-   }
-   print_unf(" ");
-  }
- }
-*/
+
  head->crc32 = ((auint)(buf[334])      ) +
                ((auint)(buf[335]) <<  8) +
                ((auint)(buf[336]) << 16) +
                ((auint)(buf[337]) << 24);
 
- head->psupport = buf[338]; /* the peripherals the ROM supports */
- if (head->psupport & PERIPHERAL_MOUSE){ sprintf((char *)psupport_str+strlen((char *)psupport_str), "Mouse,");}
+ head->psupport = buf[0x152]; /* the peripherals the ROM supports */
+ psupport_str[0] = '\0';
+ if (head->psupport & PERIPHERAL_MOUSE){ sprintf((char *)psupport_str+strlen((char *)psupport_str), "Mouse,"); }
  if (head->psupport & PERIPHERAL_KEYBOARD){ sprintf((char *)psupport_str+strlen((char *)psupport_str), "Keyboard,"); }
  if (head->psupport & PERIPHERAL_MULTITAP){ sprintf((char *)psupport_str+strlen((char *)psupport_str), "Multitap,"); }
  if (head->psupport & PERIPHERAL_ESP8266){ sprintf((char *)psupport_str+strlen((char *)psupport_str), "ESP8266,"); }
  if (strlen((char *)psupport_str) && psupport_str[strlen((char *)psupport_str)-1] == ','){ psupport_str[strlen((char *)psupport_str)-1] = '\0'; } /* remove trailing comma, if present */
+ if (!strlen((char *)psupport_str)){ sprintf((char *)psupport_str+strlen((char *)psupport_str), "None"); }
+   
 
  for (i = 0U; i < 63U; i++){ head->desc[i] = buf[339U + i]; }
  head->desc[63] = 0U;
 
- head->pdefault = buf[339U+64U+1U]; /* the peripherals that should be "connected" at start(save the user some hotkey presses) */
- if (head->pdefault & PERIPHERAL_MOUSE){ sprintf((char *)pdefault_str+strlen((char *)pdefault_str), "Mouse,");}
+ head->pdefault = buf[0x193]; /* the peripherals that should be "connected" at start(save the user some hotkey presses) */
+ pdefault_str[0] = '\0';
+ if (head->pdefault & PERIPHERAL_MOUSE){ sprintf((char *)pdefault_str+strlen((char *)pdefault_str), "Mouse,"); }
  if (head->pdefault & PERIPHERAL_KEYBOARD){ sprintf((char *)pdefault_str+strlen((char *)pdefault_str), "Keyboard,"); }
  if (head->pdefault & PERIPHERAL_MULTITAP){ sprintf((char *)pdefault_str+strlen((char *)pdefault_str), "Multitap,"); }
  if (head->pdefault & PERIPHERAL_ESP8266){ sprintf((char *)pdefault_str+strlen((char *)pdefault_str), "ESP8266,"); }
  if (strlen((char *)pdefault_str) && pdefault_str[strlen((char *)pdefault_str)-1] == ','){ pdefault_str[strlen((char *)pdefault_str)-1] = '\0'; } /* remove trailing comma, if present */
+ if (!strlen((char *)pdefault_str)){ sprintf((char *)pdefault_str+strlen((char *)pdefault_str), "None"); }
+ sprintf((char *)pdisplay_str, "%s, Default: %s", psupport_str, pdefault_str);
 
+ head->jamma = buf[0x194]; /* JAMMA options for rotation, mirroring, and future use */
+ jamma_str[0] = '\0';
+ if (head->jamma){
+   sprintf((char *)jamma_str+strlen((char *)jamma_str), "Enabled:");
+   if ((head->jamma & (JAMMA_ROTATE_90 | JAMMA_ROTATE_180 | JAMMA_ROTATE_270)) > JAMMA_ROTATE_270){
+     sprintf((char *)jamma_str, "\tError: multiple rotation bits in header");
+     head->jamma = 0;
+   }
+   else if (head->jamma & JAMMA_ROTATE_90){ sprintf((char *)jamma_str+strlen((char *)jamma_str), "Rotate 90,"); }
+   else if (head->jamma & JAMMA_ROTATE_90){ sprintf((char *)jamma_str+strlen((char *)jamma_str), "Rotate 90,"); }
+   else if (head->jamma & JAMMA_ROTATE_180){ sprintf((char *)jamma_str+strlen((char *)jamma_str), "Rotate 180,"); }
+   else if (head->jamma & JAMMA_ROTATE_270){ sprintf((char *)jamma_str+strlen((char *)jamma_str), "Rotate 270,"); }
+
+   if (head->jamma & JAMMA_FLIP_H){ sprintf((char *)jamma_str+strlen((char *)jamma_str), "Flip Horizontal,"); }
+   if (head->jamma & JAMMA_FLIP_V){ sprintf((char *)jamma_str+strlen((char *)jamma_str), "Flip Vertical,"); }
+   if (strlen((char *)jamma_str) && jamma_str[strlen((char *)jamma_str)-1] == ','){ jamma_str[strlen((char *)jamma_str)-1] = '\0'; } /* remove trailing comma, if present */
+ }else{
+   sprintf((char *)jamma_str+strlen((char *)jamma_str), "Disabled");
+ }
  rv = filesys_read(FILESYS_CH_EMU, cmem, len);
  if (rv != len){
   print_error("%s%sNot enough data for program in %s.\n", cu_id, cu_err, fname);
@@ -163,20 +189,20 @@ boole cu_ufile_load(char const* fname, uint8* cmem, cu_ufile_header_t* head)
 
  /* Print out successful result */
 
- print_message(
-  "%sSuccesfully loaded program from %s:\n", cu_id, fname);
+ print_message("%sSuccesfully loaded program from %s:\n", cu_id, fname);
  print_message(
   "\tName .......: %s\n"
   "\tAuthor .....: %s\n"
   "\tYear .......: %d\n"
   "\tDescription : %s\n"
-  "\tSupported ..: %s\n",
+  "\tSupported ..: %s\n"
+  "\tJAMMA ......: %s\n",
   (char*)(&(head->name[0])),
   (char*)(&(head->author[0])),
   head->year,
   (char*)(&(head->desc[0])),
-  (char*)(&(psupport_str)));
-
+  (char*)(&(pdisplay_str)),
+  (char*)(&(jamma_str)));
  /* Successful */
 
  return TRUE;
